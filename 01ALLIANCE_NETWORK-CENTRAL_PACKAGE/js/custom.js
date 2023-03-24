8c01fa6a57237826015dbf60cdc76e303e37cb28
Tamara Marnell <tmarnell@orbiscascade.org>
2023-03-24 08:26:42 -0700

    Add hideSummit module
    This customization hides the Resource Sharing form on the Primo VE services page for titles with no holdings in the Orbis Cascade Alliance Network Zone.

--- 01ALLIANCE_NETWORK-CENTRAL_PACKAGE/js/custom.js
+++ 01ALLIANCE_NETWORK-CENTRAL_PACKAGE/js/custom.js
@@ -1,7 +1,7 @@
 /*
 *
 *	Orbis Cascade Alliance Central Package
-*	Last updated: 2023-03-15
+*	Last updated: 2023-03-24
 *
 * Included customizations:
 *   Insert custom action (updated 2018-11-07)
@@ -17,6 +17,7 @@
 *   Show NZ and IZ MMS IDs (Added 2022-08-11)
 *   Set Focus on Hover in Send-To Menu (Added 2022-08-24)
 *   Same Tab Menu Links (Added 2022-10-24)
+*   Hide Summit Request Form (Added 2023-03-24)
 */
 
 (function(){
@@ -1174,5 +1175,189 @@
       }
     })
   /* End Same Tab Menu Links */
+  
+  // Hide the RS form for OpenURLs if...
+  // a) there is no OCLC number or ISBN, or 
+  // b) there are no holdings in the NZ
+  angular
+    .module('hideSummit', [])
+    .component('hideSummit', {
+      template: '<span ng-if="$ctrl.found_records">Held by: {{$ctrl.summit_list}}</span>',
+      controller: function ($scope, $location, $http) {
+        
+        var vm = this;
+        var parent_ctrl, oclc, isbn;
+        const sru = 'https://na01.alma.exlibrisgroup.com/view/sru/01ALLIANCE_NETWORK?version=1.2&operation=searchRetrieve&query=';
+        const institution_codes = {
+          "01ALLIANCE_NETWORK": "Orbis Cascade Alliance",
+          "01ALLIANCE_CC": "Clark College",
+          "01ALLIANCE_CCC": "Clackamas Community College",
+          "01ALLIANCE_CHEMEK": "Chemeketa Community College",
+          "01ALLIANCE_COCC": "Central Oregon Community College",
+          "01ALLIANCE_CWU": "Central Washington University",
+          "01ALLIANCE_EOU": "Eastern Oregon University",
+          "01ALLIANCE_EVSC": "Evergreen State College",
+          "01ALLIANCE_EWU": "Eastern Washington University",
+          "01ALLIANCE_GFOX": "George Fox University",
+          "01ALLIANCE_LANECC": "Lane Community College",
+          "01ALLIANCE_LCC": "Lewis & Clark College",
+          "01ALLIANCE_LINF": "Linfield College",
+          "01ALLIANCE_MHCC": "Mount Hood Community College",
+          "01ALLIANCE_OHSU": "Oregon Health and Science University",
+          "01ALLIANCE_OIT": "Oregon Institute of Technology",
+          "01ALLIANCE_OSU": "Oregon State University",
+          "01ALLIANCE_PCC": "Portland Community College",
+          "01ALLIANCE_PSU": "Portland State University",
+          "01ALLIANCE_PU": "Pacific University",
+          "01ALLIANCE_REED": "Reed College",
+          "01ALLIANCE_SEAU": "Seattle University",
+          "01ALLIANCE_SOU": "Southern Oregon University",
+          "01ALLIANCE_SPU": "Seattle Pacific University",
+          "01ALLIANCE_STMU": "Saint Martin's University",
+          "01ALLIANCE_UID": "University of Idaho",
+          "01ALLIANCE_UO": "University of Oregon",
+          "01ALLIANCE_UPORT": "University of Portland",
+          "01ALLIANCE_UPUGS": "University of Puget Sound",
+          "01ALLIANCE_UW": "University of Washington",
+          "01ALLIANCE_WALLA": "Walla Walla University",
+          "01ALLIANCE_WHITC": "Whitman College",
+          "01ALLIANCE_WOU": "Western Oregon University",
+          "01ALLIANCE_WPC": "Warner Pacific College",
+          "01ALLIANCE_WSU": "Washington State University",
+          "01ALLIANCE_WU": "Willamette University",
+          "01ALLIANCE_WW": "Whitworth University",
+          "01ALLIANCE_WWU": "Western Washington University",
+          "01WIN_GONZAGA": "Gonzaga University"
+        }
+        
+        this.$onInit = function() {
+          // For OpenURLs only
+          if ($location.path() == '/openurl') {
+            vm.found_records = false;
+            vm.parent_ctrl = $scope.$parent.$parent.$ctrl;
+            if (angular.isDefined(vm.parent_ctrl.item)) {
+              var pnx = vm.parent_ctrl.item.pnx;
+              var query = null;
+              vm.oclc = null;
+              vm.isbn = null;
+              // Check PNX for OCLC number
+              if (angular.isDefined(pnx.addata.oclcid)) {
+                vm.oclc = pnx.addata.oclcid[0];
+              }
+              // Check rft_id in URL for OCLC number or ISBN
+              else if (angular.isDefined($location.search()["rft_id"])) {
+                var rft_id = $location.search()["rft_id"];
+                if (rft_id.constructor === String) {
+                  vm.check_rft_id(rft_id);
+                }
+                else if (rft_id.constructor === Array) {
+                  angular.forEach(rft_id, function (rft_entry) {
+                    vm.check_rft_id(rft_entry);
+                  });
+                }
+              }
+              // If nothing found yet, check PNX and rft.isbn in URL for ISBN
+              if (vm.oclc == null && vm.isbn == null) {
+                if (angular.isDefined(pnx.addata.isbn)) {
+                  vm.isbn = pnx.addata.isbn[0];
+                }
+                else if (angular.isDefined($location.search()["rft.isbn"])) {
+                  vm.isbn = $location.search()["rft.isbn"];
+                }
+              }
+              // Set query prioritizing OCLC number
+              if (vm.oclc != null) {
+                query = 'alma.oclc_control_number_035_az=' + vm.oclc;
+              }
+              else if (vm.isbn != null) {
+                query = 'alma.isbn=' + vm.isbn;
+              }
+              // Hide the form if no OCLC number or ISBN anywhere
+              else {
+                console.log('No OCLC number or ISBN');
+                vm.hide_form();
+              }
+              
+              // If OCLC or ISBN, use SRU to check for NZ holdings
+              if (query != null) {
+                $http.get(sru + query).then(function (response) {
+                  var parser = new DOMParser();
+                  var xmlDoc = parser.parseFromString(response.data,"text/xml");
+                  var numRecords = xmlDoc.getElementsByTagName('numberOfRecords');
+                  if (numRecords.length > 0) {
+                    var records = numRecords[0].textContent;
+                    // If no holdings, hide the form
+                    if (records == '0') {
+                      console.log('No NZ holdings for SRU query ' + query);
+                      vm.hide_form();
+                    }
+                    // If holdings, list institutions
+                    else {
+                      var institutions = [];
+                      var holdings = xmlDoc.getElementsByTagName('datafield');
+                      angular.forEach(holdings, function(datafield) {
+                        if (datafield.getAttribute('tag') == '852') {
+                          var subfields = datafield.getElementsByTagName('subfield');
+                          angular.forEach(subfields, function(subfield) {
+                            if (subfield.getAttribute('code') == 'a') {
+                              if (angular.isDefined(institution_codes[subfield.textContent])) {
+                                institutions.push(institution_codes[subfield.textContent]);
+                              }
+                            }
+                          });
+                        }
+                      });
+                      if (institutions.length > 0) {
+                        vm.found_records = true;
+                        vm.summit_list = institutions.join(', ');
+                      }
+                      else {
+                        console.log('No institutions found in SRU query ' + query);
+                        vm.hide_form();
+                      }
+                    }
+                  }
+                  else {
+                    console.log('Error in SRU response.');
+                    console.log(sru + query);
+                    vm.hide_form();
+                  }
+                });
+              }
+            }
+          }
+        }
+        
+        // Check rft_id(s) for OCLC and ISBN
+        this.check_rft_id = function(rft_id) {
+          if (rft_id.substring(0, 12).toLowerCase() == "info:oclcnum") {
+            vm.oclc = rft_id.substr(13);
+          }
+          else if (rft_id.substring(0, 8).toLowerCase() == "urn:isbn") {
+            vm.isbn = rft_id.substr(9);
+          }
+        }
+        
+        // Remove the RS service after servicesListIsLoading gets set to false
+        this.hide_form = function() {
+          $scope.$watch(
+            function() {
+              return vm.parent_ctrl.servicesListIsLoading;
+            },
+            function(newValue, oldValue) {
+              if (newValue === false) {
+                angular.forEach(vm.parent_ctrl.services.serviceinfo, function(service, key) {
+                  if (service.type == 'AlmaResourceSharing') {
+                    vm.parent_ctrl.services.serviceinfo.shift(key);
+                  }
+                });
+              }
+            }
+          );
+        }
+        
+      }
+    });
+    /* End Hide Summit Form */
 
 })();
